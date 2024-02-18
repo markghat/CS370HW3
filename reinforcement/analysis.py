3f3ee10be9521bc120413153e5e893c73678e449
Mark Ghattas <mark.ghattas@duke.edu>
2024-02-17 21:23:45 -0500

    finished q7

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -68,8 +68,7 @@ def question3e():
 def question8():
     answerEpsilon = None
     answerLearningRate = None
-    return answerEpsilon, answerLearningRate
-    # If not possible, return 'NOT POSSIBLE'
+    return 'NOT POSSIBLE'
 
 
 if __name__ == '__main__':

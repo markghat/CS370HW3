e59197937f637cfd282c8471f086b937945ca0d3
Mark Ghattas <mark.ghattas@duke.edu>
2024-02-17 21:37:08 -0500

    finished c3

--- reinforcement/qlearningAgents.py
+++ reinforcement/qlearningAgents.py
@@ -178,14 +178,22 @@ class ApproximateQAgent(PacmanQAgent):
           where * is the dotProduct operator
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        features = self.featExtractor.getFeatures(state, action)
+        q = 0
+        for feature in features:
+            q += self.weights[feature] * features[feature]
+        return q
 
     def update(self, state, action, nextState, reward):
         """
            Should update your weights based on transition
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        dif = (reward + self.discount * self.getValue(nextState)) - \
+            self.getQValue(state, action)
+        features = self.featExtractor.getFeatures(state, action)
+        for feature in features:
+            self.weights[feature] += self.alpha * dif * features[feature]
 
     def final(self, state):
         "Called at the end of each game."
@@ -196,4 +204,5 @@ class ApproximateQAgent(PacmanQAgent):
         if self.episodesSoFar == self.numTraining:
             # you might want to print your weights here for debugging
             "*** YOUR CODE HERE ***"
+            print(self.weights)
             pass
